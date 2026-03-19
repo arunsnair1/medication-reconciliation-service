4936c47e0290ed6ccfa29dff72df00b4678aa78d
Arun_S_Nair <arunsnair23213@gmail.com>
2026-03-19 23:49:40 +0530

    Add Mongo Atlas + deployment setup

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,5 +1,9 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+
 db = client[settings.DB_NAME]
